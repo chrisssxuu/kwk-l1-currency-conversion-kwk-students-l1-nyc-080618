fe0c11a5bfe008a2c3932e4ec15197c0e836c71b
chrisssxuu <chris01.xu@gmail.com>
2018-08-08 19:28:11 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,10 @@
-# Write your cod here
+# Write your code here
+def usd_to_eur(dollars)
+  puts "How many US dollars?"
+  dollars = gets.chomp.to_f 
+end
+
+def eur_to_usd(euros)
+  puts "How many euros?"
+  euros = gets.chomp.to_f
+end
